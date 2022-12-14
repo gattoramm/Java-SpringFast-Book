6b4324988ffd6809f4cb2b3bb18fd6b83671992b
RAMIL ARSAEV <gattoramm@gmail.com>
2022-12-14 22:25:46 +0300

    Update ProjectConfig.java

--- src/main/java/part1/ch2/ex7/config/ProjectConfig.java
+++ src/main/java/part1/ch2/ex7/config/ProjectConfig.java
@@ -6,11 +6,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 // Сообщаем Spring, где находятся классы со стереотипными аннотациями
-@ComponentScan(basePackages = "part1.ch2.ex7")
+@ComponentScan(basePackages = "part1.ch2.ex7.main")
 public class ProjectConfig {
-    Parrot parrot() {
-        var p = new Parrot();
-        p.setName("Koko");
-        return p;
-    }
 }
