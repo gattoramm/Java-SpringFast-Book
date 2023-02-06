180f4b8066720dc92c5e3c53a234b83198abe035
RAMIL ARSAEV <gattoramm@gmail.com>
2023-02-06 21:30:08 +0300

    Update ProjectConfig.java

--- src/main/java/part1/ch2/ex1/config/ProjectConfig.java
+++ src/main/java/part1/ch2/ex1/config/ProjectConfig.java
@@ -5,12 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+// Описание конфигурации контекста
 public class ProjectConfig {
     // При инициализации контекста нужно вызвать данный метод и добавить в контекст возвращаемое им значение
     @Bean
     Parrot parrot() {
         var p = new Parrot();
-        p.setName("koko");
+        p.setName("Koko");
         // Spring добавляет в контекст экземпляр класса Parrot, возвращаемый методом
         return p;
     }
