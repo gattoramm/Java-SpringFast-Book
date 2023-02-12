332347aa73eba61f5d3f689dc57286ec67dd16aa
RAMIL ARSAEV <gattoramm@gmail.com>
2023-02-12 13:31:19 +0300

    Update ProjectConfig.java

--- src/main/java/part1/ch2/ex8/config/ProjectConfig.java
+++ src/main/java/part1/ch2/ex8/config/ProjectConfig.java
@@ -1,7 +1,9 @@
 package part1.ch2.ex8.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = "part1.ch2.ex8.main")
 public class ProjectConfig {
 }
