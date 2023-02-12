150efbc09af541fb5c2c0e56c60ec76a5e7fec0e
RAMIL ARSAEV <gattoramm@gmail.com>
2023-02-12 15:36:39 +0300

    добавил тесты

---
+++ src/test/java/part1/ch3/ex2/AppTest.java
@@ -0,0 +1,42 @@
+package part1.ch3.ex2;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import part1.ch3.ex2.beans.Parrot;
+import part1.ch3.ex2.beans.Person;
+import part1.ch3.ex2.config.ProjectConfig;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {ProjectConfig.class})
+public class AppTest {
+    @Autowired
+    private ApplicationContext context;
+
+    @Test
+    @DisplayName("Test that a Parrot instance has been added to the Spring context")
+    public void testKokoIsInSpringContext() {
+        var p = context.getBean(Parrot.class);
+        assertEquals("Koko", p.getName());
+    }
+
+    @Test
+    @DisplayName("Test that a Person instance has been added to the Spring context")
+    public void testEllaIsInSpringContext() {
+        var p = context.getBean(Person.class);
+        assertEquals("Ella", p.getName());
+    }
+
+    @Test
+    @DisplayName("Test that the Person instance in the Spring context doesn't own the Parrot instance from the Spring context")
+    public void testEllaDoesNotOwnKoko() {
+        var p = context.getBean(Person.class);
+        assertNotNull(p.getParrot());
+    }
+}
