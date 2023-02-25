9441991eb0611b559dd9bbad1aa2b45a9675e8a2
RAMIL ARSAEV <gattoramm@gmail.com>
2023-02-25 19:03:44 +0300

    add Tests

---
+++ src/test/java/part1/ch5/ex3/AppTests.java
@@ -0,0 +1,29 @@
+package part1.ch5.ex3;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import part1.ch5.ex3.config.ProjectConfig;
+import part1.ch5.ex3.service.CommentService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {ProjectConfig.class})
+public class AppTests {
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Test
+    @DisplayName("Verify that CommentService every time you request an instance from the Spring context, you get the same instance")
+    public void testCommentServiceIsSingleton(){
+        var cs1 = applicationContext.getBean("commentService", CommentService.class);
+        var cs2 = applicationContext.getBean("commentService", CommentService.class);
+
+        assertEquals(cs1, cs2);
+    }
+}
