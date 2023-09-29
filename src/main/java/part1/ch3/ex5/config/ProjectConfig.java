dd1f02a76d3c9dead825d67888d8ec7163778a8e
RAMIL ARSAEV <gattoramm@gmail.com>
2023-09-29 10:51:57 +0300

    Update ProjectConfig.java

--- src/main/java/part1/ch3/ex5/config/ProjectConfig.java
+++ src/main/java/part1/ch3/ex5/config/ProjectConfig.java
@@ -1,10 +1,7 @@
 package part1.ch3.ex5.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import part1.ch3.ex5.beans.Parrot;
-import part1.ch3.ex5.beans.Person;
 
 @Configuration
 @ComponentScan(basePackages = "part1.ch3.ex5.beans")
