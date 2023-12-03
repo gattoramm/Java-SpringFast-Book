0b0a4d468ed7dcbec096e7c9b7dada0830ecde83
gattoramm <gattoramm@gmail.com>
2023-12-03 21:00:55 +0300

    пример с aop (изменение метода)

---
+++ src/main/java/part1/ch6/ex2/services/CommentService.java
@@ -0,0 +1,16 @@
+package part1.ch6.ex2.services;
+
+import org.springframework.stereotype.Service;
+import part1.ch6.ex2.model.Comment;
+
+import java.util.logging.Logger;
+
+@Service
+public class CommentService {
+    private Logger logger = Logger.getLogger(CommentService.class.getName());
+
+    public String publishComment(Comment comment) {
+        logger.info("Publishing comment:" + comment.getText());
+        return "SUCCESS";
+    }
+}
